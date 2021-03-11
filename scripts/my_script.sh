6e388f3a2ccee5bf04814565bbb6d86f510152ab
benedict909 <monstruo319@gmail.com>
2021-03-11 12:15:41 +0000

    update script to make empty dirs and READMEs

--- scripts/my_script.sh
+++ scripts/my_script.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/env bash
+
+mkdir -p data analysis docs
+
+for my_dir in scripts analysis docs data;do
+	touch ${my_dir}/README.md 
+	echo "# ${my_dir}" >> ${my_dir}/README.md 
+done
+
+
